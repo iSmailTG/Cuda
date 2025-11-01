e0a5620affea0e2d07f501d39004a99faa5108a1
Ismail TG <mail.taghchti@gmail.com>
2025-11-01 23:45:08 +0100

    new stuff

--- day_04_arrays/summary.cpp
+++ day_04_arrays/summary.cpp
@@ -6,11 +6,13 @@ using namespace std;
 int main()
 {
   int num;
-  cout << "print the number: " << num << endl;
+  cout << "print the number before assigning any value to it : " << num << endl;
   num = 5;
   cout << "print the number: " << num << endl;
   cout <<"print the number address: " << &num << endl;
-
+  int *ptr;
+  ptr = &num;
+  cout << "print the address of the number through a pointer: " << ptr << endl;
 
   return 0;
 }
