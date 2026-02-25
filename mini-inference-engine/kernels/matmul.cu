d64326467c35229058bd411c0e2f61e292ee2bd0
Ismail TG <mail.taghchti@gmail.com>
2026-02-25 00:20:30 +0000

    first kernel

---
+++ mini-inference-engine/kernels/matmul.cu
@@ -0,0 +1,33 @@
+#include <cuda_runtime.h>
+
+
+__global__ void kernel_matmul(float* A, float* B, float* C,
+                              int M, int N, int K)
+{
+  int row = blockIdx.y * blockDim.y + threadIdx.y;
+  int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+  float = sum;
+  if(row < M && col < N)
+  {
+    for(int i = 0; i < K; i++)
+    {
+      sum += A[row * K + i] * B[i * N + col];
+    }
+  C[row * N + col] = sum;
+  }
+}
+
+exter "C"
+{
+  void matmul_cuda(float* A, float* B, float* C,
+                   int M, int N, int K)
+  {
+    dim3 blockIdx(16, 16);
+    dim3 blockDim((N + 15) / 16, (M + 15) / 16);
+
+    kernel_matmul <<< blockDim, blockIdx >>> (A, B, C, M, N, K);
+    cudaDeviceSynchronize();
+
+  }
+}
