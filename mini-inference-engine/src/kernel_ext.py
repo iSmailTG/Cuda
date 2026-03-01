b42cfd657974faf3cea0f1e619e1f7990cb8e137
TheSam <mail.taghchti@gmail.com>
2026-03-01 22:12:39 +0000

    add sources

--- mini-inference-engine/src/kernel_ext.py
+++ mini-inference-engine/src/kernel_ext.py
@@ -1,11 +1,12 @@
-
 from torch.utils.cpp_extension import load
 
-try:
-  matmul_ext = load(name = "matmul_ext", sources = ['kernels/matmul_ext.cu'], verbose = True)
-  print("Compilation Completed")
-except Exception as e:
-    print(f"An error occurred during compilation: {e}")
+
+matmul_ext = load(
+    name    = "matmul_ext",
+    sources = ["kernels/matmul_ext.cu"],
+    verbose = True,
+)
 
 def matmul_cuda_v2(A, B):
-  return matmul_ext.matmul(A, B)
+   
+    return matmul_ext.matmul(A, B)
