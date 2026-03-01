c65eedd8038641ba880a82cb20cf9980f383d4b7
TheSam <mail.taghchti@gmail.com>
2026-03-01 17:02:35 +0000

    add kernel_ext.py

---
+++ mini-inference-engine/src/kernel_ext.py
@@ -0,0 +1,6 @@
+
+from torch.utils import load
+matmul_ext = load(name = "matmul_ext", source = ['kernel/matmul_ext.cu'], verbose = True)
+
+def matmul_cuda_v2(A, B):
+  return matmul_ext.matmul(A, B)
