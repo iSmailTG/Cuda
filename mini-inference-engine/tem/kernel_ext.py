b1235c1702f06d585e4b52de3fab5f65d34914ce
TheSam <mail.taghchti@gmail.com>
2026-04-03 12:07:47 +0000

    replicate experiments: ctypes VS cppExtention VS torch

---
+++ mini-inference-engine/tem/kernel_ext.py
@@ -0,0 +1,10 @@
+from torch.utils.cpp_extension import load
+
+matmul_ext = load(
+    name = "matmul_ext",
+    sources = ['tem/matmul_ext.cu'],
+    verbose = True
+)
+
+def matmul_cuda_V2(A, B):
+  return matmul_ext.matmul(A, B)
