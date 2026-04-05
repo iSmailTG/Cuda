c04820f99d1182a357caa70e0adbd9fd590ce809
Ismail TG <mail.taghchti@gmail.com>
2026-04-05 14:20:22 +0100

    fix kernel_ext bugs

--- mini-inference-engine/tem/kernel_ext.py
+++ mini-inference-engine/tem/kernel_ext.py
@@ -3,7 +3,7 @@ from torch.utils.cpp_extension import load
 matmul_ext = load(
   name  = "matmul_ext",
   sources = ['tem/matmul_kernel.cu'],
-  verobes = True
+  verbose = True
 )
 
 def matmul_cuda_V2(A, B):
