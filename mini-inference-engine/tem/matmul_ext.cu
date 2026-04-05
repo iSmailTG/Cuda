8cb1a5e3410b9617aa03b8552c8c4cc34c0fcc49
Ismail TG <mail.taghchti@gmail.com>
2026-04-05 14:43:18 +0100

    fix matmul_ext bugs

--- mini-inference-engine/tem/matmul_ext.cu
+++ mini-inference-engine/tem/matmul_ext.cu
@@ -1,9 +1,9 @@
 #include <torch/extension.h>
 
-__global__ void matmul_kernel(float* A,float* B, float* C, M, N, K)
+__global__ void matmul_kernel(float* A,float* B, float* C, int M, int N, int K)
 {
   int row = blockIdx.y * blockDim.y + threadIdx.y;
-  int col = blockIdx.x * blockDim.y + threadIdx.x;
+  int col = blockIdx.x * blockDim.x + threadIdx.x;
 
   if(row < M && col < N)
   {
@@ -21,28 +21,28 @@ torch::Tensor matmul_ext(torch::Tensor A, torch::Tensor B)
 {
   TORCH_CHECK(A.is_cuda(), "must be on cuda");
   TORCH_CHECK(B.is_cuda(), "must be on cuda");
-  TORCH_CHECK(A.dtype == torch.kFloat32, "support float32 only");
+  TORCH_CHECK(A.dtype == torch::kFloat32, "support float32 only");
   TORCH_CHECK(A.is_contiguous(), "must be contiguous");
   TORCH_CHECK(B.is_contiguous(), "must be contiguous");
   TORCH_CHECK(A.size(1) == B.size(0), "A col mus match ");
 
   int M = A.size(0), K = A.size(1), N = B.size(1);
-  auto C = torch.zeros({M, N}, A.options());
+  auto C = torch::zeros({M, N}, A.options());
 
   dim3 blockDim(16, 16);
   dim3 gridDim((N + 15) / 16, (M + 15) / 16);
 
-  matmul_kernel<<<griDim, blockDim>>>(
+  matmul_kernel<<<gridDim, blockDim>>>(
     A.data_ptr<float>(),
     B.data_ptr<float>(),
     C.data_ptr<float>(),
     M, N, K);
 
   cudaDeviceSynchronize();
-  return C;    
+  return C;
 }
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
 {
-  m.def("matmul", &matmul_kernel, "Matrix Multiply (CUDA)");
+  m.def("matmul", &matmul_ext, "Matrix Multiply (CUDA)");
 }
